1c8e9186ea3d26ea8a0ba1281aa5830123ae88ba
Suchit Mishra <suchitm2050@gmail.com>
2026-09-16 22:52:05 +0530

    Removing & updating some changes

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { assets } from "../assets/assets";
 
 const Sidebar = () => {
   return (
-    <div className="w-[8%] min-h-screen border-r-2">
+    <div className="w-[18%] min-h-screen border-r-2">
       <div className="flex flex-col gap-4 pt-6 pl-[20%] text-[15px]">
         <NavLink
           to="add"
